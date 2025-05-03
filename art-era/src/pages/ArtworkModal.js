3674688984348c274b81fb2712de5308093af16b
Danniella M <167385272+danniemtz@users.noreply.github.com>
2025-05-03 13:07:18 -0700

    added names to the modal and fixed some colors

--- art-era/src/pages/ArtworkModal.js
+++ art-era/src/pages/ArtworkModal.js
@@ -19,20 +19,20 @@ export default function ArtworkModal({ artwork, onClose }) {
   if (!artwork) return null;
 
   return (
-    <div className="modal-overlay" onClick={onClose}>
-        <button className="modal-close" onClick={onClose}> X </button>
-      <div className="modal-box" onClick={(e) => e.stopPropagation()}>
+    <div className="M-modal-overlay" onClick={onClose}>
+        <button className="M-modal-close" onClick={onClose}> X </button>
+      <div className="M-modal-box" onClick={(e) => e.stopPropagation()}>
 
-        <div className="modal-image-container">
-          <img src={artwork.primaryImageSmall} alt={artwork.title} className="modal-image" />
+        <div className="M-modal-image-container">
+          <img src={artwork.primaryImageSmall} alt={artwork.title} className="M-modal-image" />
         </div>
 
-        <div className="modal-details">
-          <h2 className="modal-title">{artwork.title}</h2>
-          <p className="modal-artist"><strong>Artist:</strong> {artwork.artistDisplayName || artwork.creditLine || "Unknown"}</p>
-          <p className="modal-artist"><strong>Period: </strong> {artwork.period || "Unknown"}</p>
-          <p className="modal-artist"><strong>Date:</strong> {artwork.objectDate || "Unknown"}</p>
-          <p className="modal-description">
+        <div className="M-modal-details">
+          <h2 className="M-modal-title">{artwork.title}</h2>
+          <p className="M-modal-artist"><strong>Artist:</strong> {artwork.artistDisplayName || artwork.creditLine || "Unknown"}</p>
+          <p className="M-modal-artist"><strong>Period: </strong> {artwork.period || "Unknown"}</p>
+          <p className="M-modal-artist"><strong>Date:</strong> {artwork.objectDate || "Unknown"}</p>
+          <p className="M-modal-description">
             {artwork.creditLine?.trim() ||
              wikidataDescription?.trim() ||
              "No description available."}
@@ -40,7 +40,7 @@ export default function ArtworkModal({ artwork, onClose }) {
 
           {artwork.objectWikidata_URL && (
             <a
-              className="modal-wiki"
+              className="M-modal-wiki"
               href={artwork.objectWikidata_URL}
               target="_blank"
               rel="noopener noreferrer"
