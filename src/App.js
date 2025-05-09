8e5d0ecda61e76b1f887e2790d758bf1805df667
Danniella M <167385272+danniemtz@users.noreply.github.com>
2025-05-08 19:43:01 -0700

    Flatten project for Vercel deployment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import BrowsePage from './pages/BrowsePage'; // optional if you already have it
+import BrowsePage from './pages/BrowsePage';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/ArtEra">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/browse" element={<BrowsePage />} />
